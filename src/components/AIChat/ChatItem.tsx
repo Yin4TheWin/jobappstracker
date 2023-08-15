2e91fabe32a87b0f5d71f8b98ce264bfa32660dc
Franklin Yin <yin4thewin@gmail.com>
2023-08-15 03:55:41 -0400

    feat: tidy up home page and add section on frank

--- src/components/AIChat/ChatItem.tsx
+++ src/components/AIChat/ChatItem.tsx
@@ -7,7 +7,7 @@ export default function ChatItem({msg, author}: MessageType){
     return(<Fragment>
         <p className={author.toLowerCase().replaceAll(".", "")}>{author}</p>
         <div style={{display: 'flex', flexDirection: 'row'}}>
-            <p style={{textAlign: 'left', color:author.toLowerCase()==='you'?'black':'rgb(1, 36, 154)', width: '85vw'}}>{msg}</p>
+            <p style={{textAlign: 'left', color:author.toLowerCase()==='you'?'black':'rgb(1, 36, 154)', width: '85vw'}}>{msg.split("\n").map(str=><p style={{textAlign: 'left', color:author.toLowerCase()==='you'?'black':'rgb(1, 36, 154)', width: '85vw'}}>{str}</p>)}</p>
         </div>
         {/* {extractLinkFromText(message, author)} */}
         <hr style={{width: '90vw'}}/>
